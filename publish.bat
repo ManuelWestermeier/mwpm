6c92598705c565d88dfdefa1b8f1eb17da4dccb1
ManuelWestermeier <westermeier111@gmail.com>
2024-07-04 16:13:00 +0200

    update

--- publish.bat
+++ publish.bat
@@ -1,6 +1,4 @@
-git init
 git add .
-git commit -m "first commit"
+git commit -m "update"
 git branch -M main
-git remote add origin https://github.com/ManuelWestermeier/mwpm.git
 git push -u origin main
